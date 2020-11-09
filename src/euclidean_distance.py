817aa117ffd55e84a363bef0900ab70cdfbbf334
Jonathan <jc09310@gmail.com>
2020-11-08 22:20:58 -0500

    Vectors created

--- src/euclidean_distance.py
+++ src/euclidean_distance.py
@@ -5,6 +5,15 @@ Create vectors for each player and calculate the distance between each vector
 """
 
 df = pd.read_csv('stats.csv')
+player_dictionary = {}
+vector= []
+for index, row in df.iterrows():
+    name = row[0]
+    player_dictionary[name] = row[4:].array
+
+lebron = player_dictionary["LeBron James"]
+luka = player_dictionary["Luka Dončić"]
 
 
-
+print(lebron[0:])
+print(luka[0:])
