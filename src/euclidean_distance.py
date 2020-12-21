12e13b1fe27bd45bfbde46bd86b20cc6af7e086e
Jonathan <jc09310@gmail.com>
2020-12-20 21:22:18 -0500

    Using Pandas Df instead now

--- src/euclidean_distance.py
+++ src/euclidean_distance.py
@@ -5,15 +5,21 @@ Create vectors for each player and calculate the distance between each vector
 """
 
 df = pd.read_csv('stats.csv')
-player_dictionary = {}
-vector= []
-for index, row in df.iterrows():
-    name = row[0]
-    player_dictionary[name] = row[4:].array
+df = df.drop(columns=['Pos', "Age", "Tm", "G", "GS"])
+df = df.fillna(0)
+df.set_index('Player', inplace=True)
+lst = df.loc['Steven Adams']
+print(lst['MP'])
+ 
+#player_dictionary = {}
+#vector= []
+#for index, row in df.iterrows():
+#    name = row[0]
+#    player_dictionary[name] = row[4:].array
 
-lebron = player_dictionary["LeBron James"]
-luka = player_dictionary["Luka Dončić"]
+#lebron = player_dictionary["LeBron James"]
+#luka = player_dictionary["Luka Dončić"]
 
 
-print(lebron[0:])
-print(luka[0:])
+#print(lebron[0:])
+#print(luka[0:])
--- src/get_stats.py
+++ src/get_stats.py
@@ -12,7 +12,6 @@ year = 2020
 # URL page we will scraping
 url = "https://www.basketball-reference.com/leagues/NBA_2020_per_game.html".format(
     year)
-# this is the HTML from the given URL
 html = urlopen(url)
 soup = BeautifulSoup(html, features="html.parser")
 # use findALL() to get the column headers
@@ -21,6 +20,7 @@ soup.findAll('tr', limit=2)
 headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
 # exclude the first column as we will not need the ranking order from Basketball Reference for the analysis
 headers = headers[1:]
+
 
 # avoid the first header row
 rows = soup.findAll('tr')[1:]
@@ -36,8 +36,9 @@ for i in range(len(player_stats_raw)):
             if stats[len(stats)-1][0] != player_stats_raw[i][0]:
                 stats.append(player_stats_raw[i])
      
-        
-    
 
 data = pd.DataFrame(stats, columns=headers)
 data.to_csv('stats.csv', index = False)
+    
+
+
