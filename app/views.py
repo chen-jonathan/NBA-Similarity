1dfd992b25990dff10975e18be2dc918d2296be8
Jiongan Mu <jiongan.mu@mail.utoronto.ca>
2020-12-24 11:48:46 -0500

    fixed Django form and made the website look nicer

--- app/views.py
+++ app/views.py
@@ -1,14 +1,37 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
+from django.urls import reverse
 
-class NewTaskForm(forms.Form):
-    task = forms.CharField(label="Player:")
+
+
+
+class NameForm(forms.Form):
+    player_real_name = forms.CharField(label=False)
 
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, "app/index.html")
+    # Check if method is POST
+
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():  # maybe add if player is in dictionary
+            # process the data in form.cleaned_data as required
+            player_name = form.cleaned_data["player_real_name"]
+            return render(request, 'app/index.html', {'form': form, 'player_name': player_name, 'submitted': True})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
 
 
-def player(request):
-    return render(request, "app/player.html")
+    return render(request, 'app/index.html', {'form': form, 'player_name': "", 'submitted': False})
+
+
+
--- nbaSimilarity/settings.py
+++ nbaSimilarity/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,3 +119,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
